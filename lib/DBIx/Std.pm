e09bf1035729f9307b0b1c3dfa90dfc99a0d5ff7
Daniel Muey <dmuey@cpan.org>
2006-09-11 21:42:58 +0000

    Import of DMUEY/DBIx-Std-v0.0.1 from CPAN.
    gitpan-cpan-distribution: DBIx-Std
    gitpan-cpan-version:      v0.0.1
    gitpan-cpan-path:         DMUEY/DBIx-Std-v0.0.1.tar.gz
    gitpan-cpan-author:       DMUEY
    gitpan-cpan-maturity:     released

---
+++ lib/DBIx/Std.pm
@@ -0,0 +1,40 @@
+package DBIx::Std;
+
+use strict;
+use warnings;
+
+use version;our $VERSION = qv('0.0.1');
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+DBIx::Std - Placeholder while its written
+
+=head1 SYNOPSIS
+
+  use DBIx::Std;
+
+=head1 DESCRIPTION
+
+Placeholder while its written, thanks for your patience
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Daniel Muey
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 by Daniel Muey
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.6 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
